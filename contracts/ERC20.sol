180c5a67dc46cc79b0ef932568c5bd53ac7bd03b
huzaifahanif2112305 <bcsbs2112305@szabist.pk>
2025-02-27 13:07:53 +0500

    second commit

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,6 +4,8 @@ import"@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import"@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 
-contract MyToken is ERC20, ERC20Permit{
+contract huzaifa is ERC20, ERC20Permit{
+constructor() ERC20("Huzaifa","hhr") ERC20Permit("huzaifa"){
     
+}
 }
